580f1a6b327e1513853977c635cff975dd818df1
eaniko <69327188+eaniko@users.noreply.github.com>
2021-11-03 23:34:03 +0100

    js 02 első

--- js/first.js
+++ js/first.js
@@ -4,3 +4,14 @@
 - A függvény szorozza meg a tömbelemek értékét 1.27-tel, majd pedig 
 __térjen vissza__ az elemek egész számra kerekített összegével!
 */
+
+const arr = [1, 2, 3, 4];
+function brutto(array) {
+	array = array.forEach((element) => element * 1.27);
+	array = array.reduce(
+		(previousValue, currentValue) => previousValue + currentValue
+	);
+	return array;
+}
+
+console.log(brutto(arr));
